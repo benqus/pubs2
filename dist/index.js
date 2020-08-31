51df71f4a55d6d9dd30ee03c5ddbc438a3a4d318
Bence Kormos <ben.kormos@gmail.com>
2020-09-01 00:13:46 +0100

    outfile

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,80 @@
-/// <reference path="./types.d.ts" />
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "./publish", "./subscribe", "./unsubscribe"], factory);
-    }
-})(function (require, exports) {
+define("publish", ["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    var publish_1 = require("./publish");
-    var subscribe_1 = require("./subscribe");
-    var unsubscribe_1 = require("./unsubscribe");
-    function default_1() {
+    function default_1(subs) {
+        return function publish(_a, payload) {
+            var o = _a[0], a = _a[1];
+            if (!subs.has(o))
+                return;
+            var actions = subs.get(o);
+            if (!actions.get(a))
+                return;
+            var subscriptions = actions.get(a);
+            if (subscriptions)
+                subscriptions.forEach(function (fn) { return fn(o, a, payload); });
+        };
+    }
+    exports.default = default_1;
+});
+define("subscribe", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function default_2(subs) {
+        return function subscribe(_a, fn) {
+            var o = _a[0], a = _a[1];
+            // create object-space
+            if (!subs.has(o))
+                subs.set(o, new Map());
+            var context = subs.get(o);
+            if (!a)
+                throw new Error('Action must be provided!');
+            // crate action-space
+            if (!context.has(a))
+                context.set(a, new Set());
+            context.get(a).add(fn);
+            return fn;
+        };
+    }
+    exports.default = default_2;
+});
+define("unsubscribe", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function default_3(subs) {
+        function _removeContext(o) {
+            var actions = subs.get(o);
+            actions.forEach(function (set) { return set.clear(); });
+            actions.clear();
+            subs.delete(o);
+        }
+        return function unsubscribe(_a, fn) {
+            var o = _a[0], a = _a[1];
+            if (!subs.has(o))
+                return;
+            // remove all object-space subscriptions
+            if (!a)
+                _removeContext(o);
+            // remove action-space subscriptions
+            var actions = subs.get(o);
+            var subscriptions = actions.get(a);
+            // remove subscription from subscriptions
+            if (subscriptions)
+                subscriptions.delete(fn);
+            // clean up action-space
+            if (subscriptions.size === 0)
+                actions.delete(a);
+            // clean up object-space
+            if (actions.size === 0)
+                subs.delete(o);
+        };
+    }
+    exports.default = default_3;
+});
+/// <reference path="./types.d.ts" />
+define("index", ["require", "exports", "publish", "subscribe", "unsubscribe"], function (require, exports, publish_1, subscribe_1, unsubscribe_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function default_4() {
         var subs = new Map();
         return {
             subscribe: subscribe_1.default(subs),
@@ -21,5 +82,5 @@
             publish: publish_1.default(subs),
         };
     }
-    exports.default = default_1;
+    exports.default = default_4;
 });
